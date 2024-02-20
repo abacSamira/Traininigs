36401b0061df0d4ea743ded2ad9e7b8e2f270beb
Samira Abbasi <abbasi.samira67@gmail.com>
2024-02-20 08:21:25 +0330

    project is up to date

---
+++ array-identical.php
@@ -0,0 +1,13 @@
+<?php 
+$a = array(10,20,30);
+$b = array(
+    1 => 20,
+    2 => 30,
+    0 => 10,
+);
+echo '$a==$b ';
+var_dump ($a==$b);
+echo '<br/>';
+echo '$a===$b ';
+var_dump ($a===$b);//check identicaly
+echo'<br/>';
